80d92567b197a8d794a44eb8b50f27e22f9070c1
Youngmin Cho <choyoungmin0322@gmail.com>
2025-01-24 09:15:22 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 조영민 on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
